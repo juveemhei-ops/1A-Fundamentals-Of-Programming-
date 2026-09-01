71e95af738aeebca2cd5eee6fe5237dc7de9e5bc
juveemhei-ops <juveemhei@gmail.com>
2026-09-01 21:09:22 +0800

    Add files via upload

---
+++ Assignment3.java
@@ -0,0 +1,17 @@
+public class Assignment3 {
+    public static void main(String[] args ) {
+        String firstname = "Juvee Mhei";
+        String lastname = "Barraca";
+        String address = "Tayug Pangasinan";
+        String mobileNumber = "09194071581";
+        int age = 17;
+        double gwa = 92;
+
+        System.out.println("Firstname:" + firstname);
+        System.out.println("Lastname:" + lastname);
+        System.out.println("Address:" + address);
+        System.out.println("Mobile Number:" + mobileNumber);
+        System.out.println("Age:" + age);
+        System.out.println("GWA:" + gwa);
+    }
+}
