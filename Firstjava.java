826f5677b8c316d37f046cb622dda3b5a4f9473f
juveemhei-ops <juveemhei@gmail.com>
2026-09-22 22:41:27 +0800

    Add files via upload

---
+++ Firstjava.java
@@ -0,0 +1,12 @@
+public class Firstjava {
+    public static void main(String[] args) {
+        String name1 = "Sarah Kaye B. Calonge";
+        String section1 = "BSIT-1A";
+        String name2 = "Juvee Mhei M. Barraca";
+        String section2 = "BSIT-1A";
+        System.out.println(name1);
+        System.out.println(section1);
+        System.out.println(name2);
+        System.out.println();
+    }
+}
---
+++ Secondjava.java
@@ -0,0 +1,25 @@
+public class Secondjava {
+    public static void main(String[] args){
+
+        String name1 = "Sarah Kaye B. Calonge";
+        String address1 = "Rosario, La Union";
+        String contactNumber1 = "09928322427";
+        int Age1 = 18;
+        String Crush1 = "Navik";
+        String name2 = "Juvee Mhei M.Barraca";
+        String address2 = "Carriedo , Tayug Pangasinan";
+        String contactNumber2 = "09194071581";
+        int Age2 = 17;
+        String Crush2 = "N/A";
+        System.out.println(name1);
+        System.out.println(address1);
+        System.out.println(contactNumber1);
+        System.out.println(Age1);
+        System.out.println(Crush1);
+        System.out.println(name2);
+        System.out.println(address2);
+        System.out.println(contactNumber2);
+        System.out.println(Age2);
+        System.out.println(Crush2);
+    }
+}
